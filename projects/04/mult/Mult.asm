69310a5cc223be2b2138182e4a6049d69c5cb2f5
serramatutu <lucasvvop@hotmail.com>
2021-04-26 22:26:05 -0300

    projeto 4 - Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,54 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Pseudocódigo
+// mult = 0
+// i = 0
+// LOOP:
+//     if i == R1 goto STOP
+//     mult += R0
+//     i++
+//     goto LOOP
+// STOP:
+//     R2 = mult
+// end
+
+@mult
+M=0     // mult = 0
+
+@i
+M=0     // i = 0
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JEQ // if i == R1 goto STOP
+
+  @mult
+  D=M
+  @R0
+  D=D+M
+  @mult
+  M=D   // mult += R0
+
+  @i
+  D=M
+  D=D+1
+  M=D   // i++
+
+  @LOOP
+  0;JMP // goto LOOP
+
+(STOP)
+  @mult
+  D=M
+  @R2
+  M=D   // R2 = mult
+
+(END)
+@END
+0;JMP
