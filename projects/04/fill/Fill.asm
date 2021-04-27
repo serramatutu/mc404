621e460cfedceb59a5926220792a88155a5040ad
serramatutu <lucasvvop@hotmail.com>
2021-04-27 00:12:13 -0300

    projeto 4 - Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,77 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Pseudocódigo
+// screen_end = SCREEN + 16384 # 16384 = (512*256/WORD_SIZE)
+// LISTENER:
+//   if KBD == 0:
+//     color = 0  # white
+//   else:
+//     color = -1   # black
+//
+//   goto PAINT
+// 
+// PAINT:
+//   pixels = SCREEN
+//   PAINT_LOOP:
+//     Mem[pixels] = color
+//     pixels++
+//     if pixels >= screen_end goto LISTENER
+//     goto PAINT_LOOP
+
+@8192
+D=A
+@SCREEN
+D=D+A
+@screen_end
+M=D                        // screen_end = SCREEN + 16384
+
+(LISTENER)
+  @KBD
+  D=M
+  @IF_COLOR_BLACK
+  D;JNE
+  (IF_COLOR_WHITE)         // if KBD == 0:
+    @color
+    M=0                    // color = 0
+    @IF_COLOR_END
+    0;JMP
+  (IF_COLOR_BLACK)         // else:
+    @color
+    M=-1                   // color = -1
+  (IF_COLOR_END)
+
+  @PAINT
+  0;JMP
+
+  @LISTENER
+  0;JMP                    // goto LISTENER
+
+
+
+(PAINT)
+  @SCREEN
+  D=A
+  @pixels
+  M=D                      // pixels = SCREEN
+  
+  (PAINT_LOOP)
+    @color
+    D=M
+    @pixels
+    A=M
+    M=D                    // Mem[pixels] = color
+
+    @pixels
+    M=M+1                  // pixels++
+
+    @pixels
+    D=M
+    @screen_end
+    D=D-M
+    @LISTENER
+    D;JGE                  // if pixels >= screen_end goto LISTENER 
+     
+    @PAINT_LOOP
+    0;JMP                  // goto PAINT_LOOP
